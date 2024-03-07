35a762ad650433683f582e5610c638a6a85a5456
xjsongphy <m18892286119@163.com>
2024-03-07 11:35:24 +0800

    update

--- update.bat
+++ update.bat
@@ -1,8 +1,9 @@
 git config --global --unset http.proxy
 git config --global --unset https.proxy
 git add .
-git commit -m "first commit"
+git commit -m "update"
 git branch -M master
 git remote add origin https://github.com/xjsongphy/2024spring-cs201.git
 git push -u origin master
 pause
+
